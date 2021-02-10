d3fafad205aeb4916944f6a3c974af9e02df0ef0
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-02-10 20:19:30 +0100

    Check upgrade available for sfdx-plugin (#16)
    * Check upgrade available for sfdx-plugin
    
    * yaml-lint config
    
    * typo & remove yaml-lint config
    
    * apply linter fixes

---
+++ src/hooks/init/check-upgrade.ts
@@ -0,0 +1,25 @@
+import * as readPkgUp from 'read-pkg-up';
+import * as updateNotifier from 'update-notifier';
+
+export const hook = async (options: any) => {
+    // Skip hooks from other commands than hardis commands
+    const commandId = options?.id || '';
+    if (!commandId.startsWith('hardis')) {
+        return;
+    }
+
+    // Check if an upgrade of sfdx-hardis is required
+    // Use promise + then to not block plugin execution during that
+    const pkg = await readPkgUp();
+    updateNotifier({
+        pkg: pkg.packageJson,
+        updateCheckInterval: 900 // check every 15 mn
+    }).then((notifier: any) => {
+        if (notifier.update) {
+            console.warn('***********************************************************************************************************************');
+            console.warn(`WARNING: You are using sfdx-hardis v${notifier.update.current}: Please upgrade to ${notifier.update.latest} by running "sfdx plugins:install sfdx-hardis"`);
+            console.warn('***********************************************************************************************************************');
+        }
+    });
+
+};
