46bb9d1ba82ce33201c6baf1ca189eb5bf40f541
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-03-09 16:40:19 +0100

    fix purge flow

--- src/commands/hardis/org/purge/flow.ts
+++ src/commands/hardis/org/purge/flow.ts
@@ -124,15 +124,16 @@ export default class OrgPurgeFlow extends SfdxCommand {
       fail: true
     });
 
+    const recordsRaw = flowQueryRes?.result?.records || flowQueryRes.records || []
     // Check empty result
-    if (!flowQueryRes.records || flowQueryRes.records.length <= 0) {
+    if (recordsRaw.length === 0) {
       const outputString = `[sfdx-hardis] No matching Flow records found with query ${query}`;
-      this.ux.log(outputString);
+      uxLog(this,c.yellow(outputString));
       return { deleted: [], outputString };
     }
 
     // Simplify results format & display them
-    const records = flowQueryRes.records.map(record => {
+    const records = recordsRaw.map((record: any) => {
       return {
         Id: record.Id,
         MasterLabel: record.MasterLabel,
@@ -141,10 +142,8 @@ export default class OrgPurgeFlow extends SfdxCommand {
         Status: record.Status
       };
     });
-    this.ux.log(
-      `[sfdx-hardis] Found ${c.bold(records.length)} records:\n${c.grey(
-        columnify(records)
-      )}`
+    uxLog(this,
+      `[sfdx-hardis] Found ${c.bold(records.length)} records:\n${c.yellow(columnify(records))}`
     );
 
     // Perform deletion
@@ -209,7 +208,7 @@ export default class OrgPurgeFlow extends SfdxCommand {
             deleted
           )}`
         : '[sfdx-hardis] No record to delete';
-    this.ux.log(c.green(summary));
+    uxLog(this,c.green(summary));
     // Return an object to be displayed with --json
     return { orgId: this.org.getOrgId(), outputString: summary };
   }
