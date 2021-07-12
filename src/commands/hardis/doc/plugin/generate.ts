d90cdcf132a80e9e562dedfaff47baf4a3d1b19a
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-07-12 13:55:59 +0200

    doc

--- src/commands/hardis/doc/plugin/generate.ts
+++ src/commands/hardis/doc/plugin/generate.ts
@@ -24,6 +24,7 @@ export default class DocPluginGenerate extends SfdxCommand {
   public static description = `Generate Markdown documentation ready for HTML conversion with mkdocs
 
 After the first run, you need to update manually:
+
 - mkdocs.yml
 - .github/workflows/build-deploy-docs.yml
 
