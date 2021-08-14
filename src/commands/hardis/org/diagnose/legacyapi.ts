4993bb732a8759cb9a6d66dcba9f668a6a167c24
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-08-14 12:04:57 +0200

    Refactor hardis:org:diagnose:legacyapi

--- src/commands/hardis/org/diagnose/legacyapi.ts
+++ src/commands/hardis/org/diagnose/legacyapi.ts
@@ -6,8 +6,9 @@ import axios from "axios";
 import * as fs from "fs-extra";
 import * as c from "chalk";
 import * as os from "os";
+import * as ObjectsToCsv from 'objects-to-csv';
 import path = require("path");
-import { execCommand, uxLog } from "../../../../common/utils";
+import { createTempDir, execCommand, uxLog } from "../../../../common/utils";
 
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
@@ -27,6 +28,16 @@ export default class LegacyApi extends SfdxCommand {
   // public static args = [{name: 'file'}];
 
   protected static flagsConfig = {
+    mode: flags.string({
+      char: "m",
+      default: "jsforce",
+      description: "Detection mode: jsforce or apex",
+    }),
+    eventtype: flags.string({
+      char: "e",
+      default: "ApiTotalUsage",
+      description: "Type of EventLogFile event to analyze",
+    }),
     debug: flags.boolean({
       char: "d",
       default: false,
@@ -52,8 +63,96 @@ export default class LegacyApi extends SfdxCommand {
   /* jscpd:ignore-end */
 
   public async run(): Promise<AnyJson> {
+    const mode = this.flags.mode || "jsforce"
     this.debugMode = this.flags.debug || false;
+    if (mode === 'jsforce') {
+      return await this.runJsForce();
+    }
+    else {
+      return await this.runApex();
+    }
+  }
 
+  // Refactoring of Philippe Ozil's apex script with JsForce queries
+  private async runJsForce() {
+    const eventType = this.flags.eventtype || 'ApiTotalUsage';
+    const conn = this.org.getConnection();
+
+    // Get EventLogFile records with EventType = 'ApiTotalUsage'
+    const logCountRes = await conn.query(`SELECT COUNT() FROM EventLogFile WHERE EventType = '${eventType}'`);
+    if (logCountRes.totalSize === 0) {
+      uxLog(this, c.green(`Found no EventLogFile entry of type ${eventType}.`));
+      uxLog(this, c.green('This indicates that no legacy APIs were called during the log retention window.'));
+      return { status: 0 };
+    }
+    uxLog(this, 'Found ' + c.bold(logCountRes.totalSize) + ` ${eventType} EventLogFile entries.`);
+
+    // Fetch EventLogFiles with ApiTotalUsage entries
+    const eventLogRes: any = await conn.query(`SELECT LogFile FROM EventLogFile WHERE EventType = '${eventType}' ORDER BY CreatedDate DESC`);
+
+    // Collect legacy api calls from logs
+    const allDeadApiCalls = [];
+    const allSoonDeprecatedApiCalls = [];
+    const allEndOfSupportApiCalls = [];
+    for (const eventLogFile of eventLogRes.records) {
+      const { deadApiCalls, soonDeprecatedApiCalls, endOfSupportApiCalls } = await this.collectDeprecatedApiCalls(eventLogFile.LogFile, conn);
+      allDeadApiCalls.push(...deadApiCalls.map(item => { item.SFDX_HARDIS_SEVERITY = 'ERROR'; return item; }));
+      allSoonDeprecatedApiCalls.push(...soonDeprecatedApiCalls.map(item => { item.SFDX_HARDIS_SEVERITY = 'WARNING'; return item; }));
+      allEndOfSupportApiCalls.push(...endOfSupportApiCalls.map(item => { item.SFDX_HARDIS_SEVERITY = 'INFO'; return item; }));
+    }
+
+    // Build command result
+    let msg = "No deprecated API call has been found in the latest 99 ApiTotalUsage logs";
+    let statusCode = 0;
+    if (allDeadApiCalls.length > 0 || allSoonDeprecatedApiCalls.length > 0) {
+      msg = "Found legacy API versions in logs";
+      statusCode = 1;
+      uxLog(this, c.red(c.bold(msg)));
+    }
+    else if (allEndOfSupportApiCalls.length > 0) {
+      msg = "Found API versions in logs that will not be supported anymore in the future";
+      statusCode = 0;
+      uxLog(this, c.yellow(c.bold(msg)));
+    } else {
+      uxLog(this, c.green(msg));
+    }
+
+    // Build output CSV file
+    const tmpDir = await createTempDir();
+    const csvLogFile = path.join(tmpDir, "legacy-api-for-"+this.org.getUsername()+".csv");
+    const csv = new ObjectsToCsv(allDeadApiCalls.concat(allSoonDeprecatedApiCalls,allEndOfSupportApiCalls));
+    await csv.toDisk(csvLogFile);
+    uxLog(this,c.cyan(`Please see detailed log in ${c.bold(csvLogFile)}`))
+
+    // Return an object to be displayed with --json
+    return { status: statusCode, message: msg, csvLogFile: csvLogFile };
+  }
+
+  // GET csv log file and check for legacy API calls within
+  private async collectDeprecatedApiCalls(logFileUrl: string, conn: any) {
+    const deadApiCalls = [];
+    const soonDeprecatedApiCalls = [];
+    const endOfSupportApiCalls = [];
+    const logEntries = await conn.request(logFileUrl);
+    for (const logEntry of logEntries) {
+      const apiVersion = logEntry.API_VERSION ? parseFloat(logEntry.API_VERSION) : null;
+      // const apiType = logEntry.API_TYPE || null ;
+      const apiFamily = logEntry.API_FAMILY || null;
+      if (['SOAP', 'REST', 'BULK'].includes(apiFamily) && apiVersion <= 7.0) {
+        deadApiCalls.push(logEntry);
+      }
+      else if (['SOAP', 'REST', 'BULK'].includes(apiFamily) && apiVersion <= 20.0) {
+        soonDeprecatedApiCalls.push(logEntry);
+      }
+      else if (['SOAP', 'REST', 'BULK'].includes(apiFamily) && apiVersion <= 30.0) {
+        endOfSupportApiCalls.push(logEntry);
+      }
+    }
+    return { deadApiCalls, soonDeprecatedApiCalls, endOfSupportApiCalls };
+  }
+
+  // Run using Philippe Ozil script as anonymous apex code (has limitations on the latest 99 ApiTotalUsage logs)
+  private async runApex() {
     // Get Legacy API scanner apex code
     const tmpApexFile = path.join(os.tmpdir(), new Date().toJSON().slice(0, 10), "legacy-api-scanner.apex");
     if (!fs.existsSync(tmpApexFile)) {
