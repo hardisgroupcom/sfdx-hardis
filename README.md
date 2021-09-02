fa304a6e66115c917ee90dd77754a964c3f3ef96
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-09-02 16:45:24 +0200

    doc

--- README.md
+++ README.md
@@ -486,16 +486,16 @@ OPTIONS
                                                                                     Hardis UI integration
 
 DESCRIPTION
-  Salesforce will remove access to old API versions in the following releases
-  An external application (ESB, ETL, Web Portal, Mobile application...) using a deprecated API version will receive 
+  - Salesforce will remove access to old API versions in the following releases
+  - An external application (ESB, ETL, Web Portal, Mobile application...) using a deprecated API version will receive 
   errors as response, and this will probably break integrations business processes
+  - More info in [Salesforce Help](https://help.salesforce.com/s/articleView?id=000351312&language=en_US&mode=1&type=1) 
+  and in this [Salesforce blog post](https://t.co/uc2cobzmVi?amp=1)
 
-  | API versions  | Salesforce release  |
-  |:-------------:|:-------------------:|
-  | 7.0 to 20.0   | Summer 21           |
-  | 21.0 to 30.0  | Summer 22           |
-
-  More info at <https://help.salesforce.com/s/articleView?id=000351312&language=en_US&mode=1&type=1>
+  | API versions  | Salesforce deprecation release  |
+  |:-------------:|:-------------------------------:|
+  | 7.0 to 20.0   | Summer 21                       |
+  | 21.0 to 30.0  | Summer 22                       |
 
   - Run the command `sfdx hardis:org:diagnose:legacyapi`
 
--- src/commands/hardis/org/diagnose/legacyapi.ts
+++ src/commands/hardis/org/diagnose/legacyapi.ts
@@ -23,15 +23,14 @@ export default class LegacyApi extends SfdxCommand {
 
   public static description = `Checks if an org uses a deprecated API version\n
 
-Salesforce will remove access to old API versions in the following releases
-An external application (ESB, ETL, Web Portal, Mobile application...) using a deprecated API version will receive errors as response, and this will probably break integrations business processes
+- Salesforce will remove access to old API versions in the following releases
+- An external application (ESB, ETL, Web Portal, Mobile application...) using a deprecated API version will receive errors as response, and this will probably break integrations business processes
+- More info in [Salesforce Help](https://help.salesforce.com/s/articleView?id=000351312&language=en_US&mode=1&type=1) and in this [Salesforce blog post](https://t.co/uc2cobzmVi?amp=1)
 
-| API versions  | Salesforce release  |
-|:-------------:|:-------------------:|
-| 7.0 to 20.0   | Summer 21           |
-| 21.0 to 30.0  | Summer 22           |
-
-More info at <https://help.salesforce.com/s/articleView?id=000351312&language=en_US&mode=1&type=1>
+| API versions  | Salesforce deprecation release  |
+|:-------------:|:-------------------------------:|
+| 7.0 to 20.0   | Summer 21                       |
+| 21.0 to 30.0  | Summer 22                       |
 
 - Run the command \`sfdx hardis:org:diagnose:legacyapi\`
 
