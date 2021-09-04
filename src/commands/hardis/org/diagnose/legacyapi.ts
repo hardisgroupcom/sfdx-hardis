2b329e9418fd2d74ce1fed5c9d70da358e7b5e43
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-09-04 09:34:09 +0200

    Doc

--- src/commands/hardis/org/diagnose/legacyapi.ts
+++ src/commands/hardis/org/diagnose/legacyapi.ts
@@ -29,10 +29,10 @@ export default class LegacyApi extends SfdxCommand {
 - An external application (ESB, ETL, Web Portal, Mobile application...) using a deprecated API version will receive errors as response, and this will probably break integrations business processes
 - More info in [Salesforce Help](https://help.salesforce.com/s/articleView?id=000351312&language=en_US&mode=1&type=1) and in this [Salesforce blog post](https://t.co/uc2cobzmVi?amp=1)
 
-| API versions  | Salesforce deprecation release  |
-|:-------------:|:-------------------------------:|
-| 7.0 to 20.0   | Summer 21                       |
-| 21.0 to 30.0  | Summer 22                       |
+| API versions | Salesforce deprecation release |
+|:------------:|:------------------------------:|
+| 7.0 to 20.0  |           Summer 21            |
+| 21.0 to 30.0 |           Summer 22            |
 
 - Run the command \`sfdx hardis:org:diagnose:legacyapi\`
 
