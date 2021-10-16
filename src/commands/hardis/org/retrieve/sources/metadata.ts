c8cef5090e4df30fbc72d9edb790108bcbcc4f2f
nvuillam <nvuillam@users.noreply.github.com>
2021-10-16 18:36:24 +0000

    [Mega-Linter] Apply linters fixes

--- src/commands/hardis/org/retrieve/sources/metadata.ts
+++ src/commands/hardis/org/retrieve/sources/metadata.ts
@@ -2,7 +2,7 @@
 import { flags, SfdxCommand } from "@salesforce/command";
 import { Messages } from "@salesforce/core";
 import { AnyJson } from "@salesforce/ts-types";
-import * as c from 'chalk';
+import * as c from "chalk";
 import * as fs from "fs-extra";
 import * as path from "path";
 import { MetadataUtils } from "../../../../../common/metadata-utils";
@@ -82,19 +82,19 @@ export default class DxSources extends SfdxCommand {
     await fs.rmdir(path.join(folder, "unpackaged"), { recursive: true });
 
     const message = `[sfdx-hardis] Successfully retrieved metadatas in ${folder}`;
-    uxLog(this,message);
+    uxLog(this, message);
 
     // Post actions for monitoring CI job
-    const repoName = await git().revparse('--show-toplevel');
-    if (isCI && repoName.includes('monitoring')) {
-      uxLog(this,c.cyan("Monitoring repo detected"));
+    const repoName = await git().revparse("--show-toplevel");
+    if (isCI && repoName.includes("monitoring")) {
+      uxLog(this, c.cyan("Monitoring repo detected"));
       // Run test classes
-      uxLog(this,c.cyan("Running Apex tests..."));
-      const orgTestRes: any = await new OrgTestApex([],this.config)._run();
+      uxLog(this, c.cyan("Running Apex tests..."));
+      const orgTestRes: any = await new OrgTestApex([], this.config)._run();
       // Check usage of Legacy API versions
-      uxLog(this,c.cyan("Running Legacy API Use checks..."));
-      const legacyApiRes: any = await new LegacyApi([],this.config)._run();
-      return { orgId: this.org.getOrgId(), outputString: message , orgTestRes,legacyApiRes};
+      uxLog(this, c.cyan("Running Legacy API Use checks..."));
+      const legacyApiRes: any = await new LegacyApi([], this.config)._run();
+      return { orgId: this.org.getOrgId(), outputString: message, orgTestRes, legacyApiRes };
     }
 
     return { orgId: this.org.getOrgId(), outputString: message };
