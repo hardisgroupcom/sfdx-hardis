83b8b40c3a483079aa6707a6c3ed0dd73d5772f8
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2022-01-06 15:59:31 +0100

    Quick fixes

--- src/common/utils/orgUtils.ts
+++ src/common/utils/orgUtils.ts
@@ -262,8 +262,7 @@ export async function initPermissionSetAssignments(permSets: Array<any>, orgUser
     const assignCommand = `sfdx force:user:permset:assign -n ${permSet.name || permSet} -u ${orgUsername}`;
     const assignResult = await execSfdxJson(assignCommand, this, {
       fail: false,
-      output: false,
-      debug: this.debugMode,
+      output: false
     });
     if (assignResult?.result?.failures?.length > 0 && !assignResult?.result?.failures[0].message.includes("Duplicate")) {
       uxLog(this, c.red(`Error assigning to ${c.bold(permSet.name || permSet)}\n${assignResult?.result?.failures[0].message}`));
@@ -288,8 +287,7 @@ export async function initApexScripts(orgInitApexScripts: Array<any>, orgAlias: 
     const apexScriptCommand = `sfdx force:apex:execute -f "${apexScript}" -u ${orgAlias}`;
     await execCommand(apexScriptCommand, this, {
       fail: true,
-      output: true,
-      debug: this.debugMode,
+      output: true
     });
   }
 }
