a648eaea71306adb39591c5368aa8c6fb5f5f2cb
Clément FERNANDEZ <hello@clementfernandez.com>
2022-10-04 15:36:54 +0200

    Handle multiple files and improve output

--- src/commands/hardis/manifest/duplicate/find.ts
+++ src/commands/hardis/manifest/duplicate/find.ts
@@ -1,9 +1,12 @@
 /* jscpd:ignore-start */
 import { flags, SfdxCommand } from "@salesforce/command";
-import { Messages } from "@salesforce/core";
+import { Logger, LoggerLevel, Messages } from "@salesforce/core";
 import { AnyJson } from "@salesforce/ts-types";
 import { uxLog } from "../../../../common/utils";
 import { parseXmlFile } from "../../../../common/utils/xmlUtils";
+import { getConfig } from "../../../../config";
+import * as glob from "glob-promise";
+import { basename } from "path";
 
 // Initialize Messages with the current plugin directory
 Messages.importMessagesDirectory(__dirname);
@@ -12,17 +15,54 @@ Messages.importMessagesDirectory(__dirname);
 // or any library that is using the messages framework can also be loaded this way.
 
 export default class Find extends SfdxCommand {
+  protected static manifestDuplicateFindKeys = {
+    layout: ["Layout.layoutSections.layoutColumns.layoutItems.field", "Layout.quickActionListItems.quickActionName"],
+    profile: ["Profile.fieldPermissions.field"],
+  };
+
   public static title = "XML duplicate values finder";
+  public static description = `find duplicate values in XML file(s).
+  Find duplicate values in XML file(s). Keys to be checked can be configured in \`config/sfdx-hardis.yml\` using property manifestDuplicateFindKeys.
 
-  public static description = "Finds duplicate values in XML file(s)";
+Default config :
+manifestDuplicateFindKeys :
+${Find.manifestDuplicateFindKeys}
+`;
 
-  public static examples = ["$ sfdx hardis:manifest:duplicate:find --file 'path/to/metadata.xml' "];
+  public static examples = [
+    `
+<?xml version="1.0" encoding="UTF-8"?>
+<Layout xmlns="http://soap.sforce.com/2006/04/metadata">
+  <layoutSections>
+      ...
+      <layoutColumns>
+          <layoutItems>
+              <behavior>Required</behavior>
+              <field>Name</field>
+          </layoutItems>
+          <layoutItems>
+              <behavior>Required</behavior>
+              <field>Name</field>
+          </layoutItems>
+      </layoutColumns>
+    </layoutSections>
+</Layout>
+`,
+    `
+$ sfdx hardis:manifest:duplicate:find --file layout.layout-meta.xml
+[sfdx-hardis] hardis:manifest:duplicate:find execution time 0:00:00.307
+[sfdx-hardis] Duplicate value found for key Layout.layoutSections.layoutColumns.layoutItems.field : Name 
+`,
+  ];
+
+  protected configInfo: any;
+  protected logLevel: LoggerLevel;
 
   protected static flagsConfig = {
-    file: flags.string({
+    files: flags.array({
       char: "f",
-      description: "XML metadata file path",
-    })
+      description: "XML metadata files path",
+    }),
   };
 
   // Comment this out if your command does not require an org username
@@ -32,52 +72,72 @@ export default class Find extends SfdxCommand {
   // protected static supportsDevhubUsername = true;
 
   // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-  //protected static requiresProject = true;
-
-  protected static uniqueKeys = {
-    layout: ["Layout.layoutSections.layoutColumns.layoutItems.field", "Layout.quickActionListItems.quickActionName"],
-    profile: ["Profile.fieldPermissions.field"],
-  };
+  protected static requiresProject = true;
 
   public async run(): Promise<AnyJson> {
+    await this.initConfig();
     this.findDuplicates();
     return;
   }
 
+  async initConfig() {
+    this.configInfo = await getConfig("user");
+    if (this.configInfo.manifestDuplicateFindKeys) {
+      Find.manifestDuplicateFindKeys = this.configInfo.manifestDuplicateFindKeys;
+    }
+    // Gets the root sfdx logger level
+    this.logLevel = (await Logger.root()).getLevel();
+  }
+
   async findDuplicates() {
     // Collect input parameters
-    const inputFile = this.flags.file;
+    const inputFiles = [];
 
-    // Extract given metadata type based on filename using type-meta.xml
-    // For example PersonAccount.layout-meta.xml returns layout and Admin.profile-meta.xml returns profile
-    const filenameRegex = /\w*\.(\w*)-meta.xml/;
-    const type = inputFile.match(filenameRegex)[1];
-
-    // Check if given metadata type has unicity rules
-    const uniqueKeys = Find.uniqueKeys[type];
-    if (!uniqueKeys) {
-      uxLog(this, `No unicity rule found for metadata type ${type}`);
-      return;
+    if (this.flags.files) {
+      const files = await glob("./" + this.flags.files, { cwd: process.cwd() });
+      inputFiles.push(...files);
     }
 
-    // Read manifest file
-    const file = await parseXmlFile(inputFile);
-    uniqueKeys.forEach((key) => {
-      // Traverse the file down to the key based on the fragments separated by . (dots), abort if not found
-      const allProps = key.split(".");
-      const valuesFound = this.traverseDown(file, allProps[0], allProps, []);
+    for (const inputFile of inputFiles) {
+      // Extract given metadata type based on filename using type-meta.xml
+      // For example PersonAccount.layout-meta.xml returns layout and Admin.profile-meta.xml returns profile
+      const filenameRegex = /\w*\.(\w*)-meta.xml/;
+      const type = inputFile.match(filenameRegex)[1];
 
-      // https://stackoverflow.com/a/840808
-      const duplicates = valuesFound.filter((e, i, a) => a.indexOf(e) !== i);
-      if (duplicates.length) {
-        uxLog(this, `Duplicate value found for key ${key} : ${duplicates}`);
+      // Check if given metadata type has unicity rules
+      const uniqueKeys = Find.manifestDuplicateFindKeys[type];
+      if (!uniqueKeys) {
+        if (this.logLevel === LoggerLevel.DEBUG) {
+          uxLog(this, `No unicity rule found for metadata type ${type} (processing ${inputFile})`);
+        }
+        continue;
       }
-    });
+
+      // Read manifest file
+      const file = await parseXmlFile(inputFile);
+      uniqueKeys.forEach((key) => {
+        // Traverse the file down to the key based on the fragments separated by . (dots), abort if not found
+        const allProps = key.split(".");
+        const valuesFound = this.traverseDown(file, allProps[0], allProps, []);
+
+        // https://stackoverflow.com/a/840808
+        const duplicates = valuesFound.filter((e, i, a) => a.indexOf(e) !== i);
+        if (duplicates.length) {
+          uxLog(
+            this,
+            `Duplicate values in ${basename(inputFile)}
+  - Key    : ${key}
+  - Values : ${duplicates.toString().replace(',', ', ')}
+`
+          );
+        }
+      });
+    }
   }
 
   /**
    *  Traverse down a XML tree, allProps containing all the properties to be traversed, currentProp being updated as we
-   * descend. 
+   * descend.
    */
   traverseDown(parent: Record<string, unknown> | Array<any>, currentProp: string, allProps: Array<string>, results: Array<string>) {
     const nextProp = allProps[allProps.indexOf(currentProp) + 1];
