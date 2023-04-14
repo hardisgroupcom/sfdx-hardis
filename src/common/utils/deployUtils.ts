6a00d67368b7919d4e9964c78d7bfac212209d43
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2023-04-14 09:39:49 +0200

    Je tente

--- src/common/utils/deployUtils.ts
+++ src/common/utils/deployUtils.ts
@@ -460,7 +460,7 @@ export async function buildDeployOnChangePackageXml(debugMode: boolean, options:
   });
 
   // Now that the diff is computed, we can dump the temporary commit
-  await git().reset(ResetMode.HARD, ["HEAD~1"]);
+  await git().reset(["HEAD~1","--hard"]);
 
   // Check git delta is ok
   const diffPackageXml = path.join(tmpDir, "package", "package.xml");
