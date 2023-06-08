79053a4328fe1834fed08586cd41fa9930b73ddd
maximeg44 <maximeg44@users.noreply.github.com>
2023-06-08 12:32:34 +0000

    [Mega-Linter] Apply linters fixes

--- README.md
+++ README.md
@@ -2470,7 +2470,7 @@ OPTIONS
   -f, --folder=folder                                                               [default: force-app] Root folder
 
 
-  -n, --namespace=namespace                                                         [default: http://soap.sforce.com/200
+  -n, --namespace=namespace                                                         [default: <http://soap.sforce.com/200>
                                                                                     6/04/metadata] XML Namespace to use
 
 
@@ -2621,8 +2621,8 @@ Deploy SFDX source to org, following deploymentPlan in .sfdx-hardis.yml
 ```
 USAGE
 
-  $ sfdx hardis:project:deploy:sources:dx [-c] [-l NoTestRun|RunSpecifiedTests|RunLocalTests|RunAllTestsInOrg] [-p 
-  <string>] [--delta] [-d] [--websocket <string>] [--skipauth] [-u <string>] [--apiversion <string>] [--json] 
+  $ sfdx hardis:project:deploy:sources:dx [-c] [-l NoTestRun|RunSpecifiedTests|RunLocalTests|RunAllTestsInOrg] [-p
+  <string>] [--delta] [-d] [--websocket <string>] [--skipauth] [-u <string>] [--apiversion <string>] [--json]
   [--loglevel trace|debug|info|warn|error|fatal|TRACE|DEBUG|INFO|WARN|ERROR|FATAL]
 
 
@@ -2668,15 +2668,15 @@ DESCRIPTION
 
   If necessary,you can define the following files (that supports wildcards <members>*</members>):
 
-  - `manifest/packageDeployOnce.xml`: Every element defined in this file will be deployed only if it is not existing yet
+- `manifest/packageDeployOnce.xml`: Every element defined in this file will be deployed only if it is not existing yet
    in the target org (can be useful with ListView for example, if the client wants to update them directly in production
    org)
-  - `manifest/packageXmlOnChange.xml`: Every element defined in this file will not be deployed if it already has a 
+- `manifest/packageXmlOnChange.xml`: Every element defined in this file will not be deployed if it already has a
   similar definition in target org (can be useful for SharingRules for example)
 
   See more in [Overwrite management documentation](https://sfdx-hardis.cloudity.com/salesforce-ci-cd-config-overwrite/)
 
-  ### Deployment plan
+### Deployment plan
 
 
   If you need to deploy in multiple steps, you can define a property `deploymentPlan` in `.sfdx-hardis.yml`.
@@ -3039,15 +3039,15 @@ OPTIONS
                                                                                     Hardis UI integration
 
 DESCRIPTION
-  - **Install packages**
-    - Use property `installedPackages`
-  - **Push sources**
-  - **Assign permission sets**
-    - Use property `initPermissionSets`
-  - **Run apex initialization scripts**
-    - Use property `scratchOrgInitApexScripts`
-  - **Load data**
-    - Use property `dataPackages`
+- **Install packages**
+  - Use property `installedPackages`
+- **Push sources**
+- **Assign permission sets**
+  - Use property `initPermissionSets`
+- **Run apex initialization scripts**
+  - Use property `scratchOrgInitApexScripts`
+- **Load data**
+  - Use property `dataPackages`
 
 EXAMPLE
   $ sfdx hardis:scratch:create
@@ -3599,7 +3599,7 @@ DESCRIPTION
   `config/.sfdx-hardis.yml`):
   - (and for scratch org only for now):
     - **Install packages**
-        - Use property `installedPackages`
+      - Use property `installedPackages`
       - **Push sources**
       - **Assign permission sets**
         - Use property `initPermissionSets`
@@ -3697,7 +3697,7 @@ When a work task is completed, guide user to create a merge request
 ```
 USAGE
   $ sfdx hardis:work:save [-n] [-g] [-c] [--auto] [--targetbranch <string>] [-d] [--websocket <string>] [--skipauth] [-u
-   <string>] [--apiversion <string>] [--json] [--loglevel 
+   <string>] [--apiversion <string>] [--json] [--loglevel
   trace|debug|info|warn|error|fatal|TRACE|DEBUG|INFO|WARN|ERROR|FATAL]
 
 OPTIONS
@@ -3732,12 +3732,12 @@ OPTIONS
 
 DESCRIPTION
 
-  - Generate package-xml diff using sfdx-git-delta
-  - Automatically update `manifest/package.xml` and `manifest/destructiveChanges.xml` according to the committed updates
-  - Automatically Clean XML files using `.sfdx-hardis.yml` properties
-    - `autocleantypes`: List of auto-performed sources cleanings, available on command 
+- Generate package-xml diff using sfdx-git-delta
+- Automatically update `manifest/package.xml` and `manifest/destructiveChanges.xml` according to the committed updates
+- Automatically Clean XML files using `.sfdx-hardis.yml` properties
+  - `autocleantypes`: List of auto-performed sources cleanings, available on command
   [hardis:project:clean:references](https://sfdx-hardis.cloudity.com/hardis/project/clean/references/)
-    - `autoRemoveUserPermissions`: List of userPermission to automatically remove from profile metadatas
+  - `autoRemoveUserPermissions`: List of userPermission to automatically remove from profile metadatas
 
   Example:
 
--- src/commands/hardis/lint/unused.ts
+++ src/commands/hardis/lint/unused.ts
@@ -15,178 +15,174 @@ Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages("sfdx-hardis", "org");
 
 export default class Unused extends SfdxCommand {
-	public static title = "check permission access";
-	public static description = "Check if elements(apex class and field) are at least in one permission set";
-	public static examples = [
-		"$ sfdx hardis:lint:access",
-		'$ sfdx hardis:lint:access -e "ApexClass:ClassA, CustomField:Account.CustomField"',
-		'$ sfdx hardis:lint:access -i "PermissionSet:permissionSetA, Profile"',
-	];
-/* jscpd:ignore-start */
-	protected static flagsConfig = {
-		debug: flags.boolean({
-			char: "d",
-			default: false,
-			description: messages.getMessage("debugMode"),
-		}),
-		websocket: flags.string({
-			description: messages.getMessage("websocket"),
-		}),
-		skipauth: flags.boolean({
-			description: "Skip authentication check when a default username is required",
-		}),
-	};
-	/* jscpd:ignore-end */
+  public static title = "check permission access";
+  public static description = "Check if elements(apex class and field) are at least in one permission set";
+  public static examples = [
+    "$ sfdx hardis:lint:access",
+    '$ sfdx hardis:lint:access -e "ApexClass:ClassA, CustomField:Account.CustomField"',
+    '$ sfdx hardis:lint:access -i "PermissionSet:permissionSetA, Profile"',
+  ];
+  /* jscpd:ignore-start */
+  protected static flagsConfig = {
+    debug: flags.boolean({
+      char: "d",
+      default: false,
+      description: messages.getMessage("debugMode"),
+    }),
+    websocket: flags.string({
+      description: messages.getMessage("websocket"),
+    }),
+    skipauth: flags.boolean({
+      description: "Skip authentication check when a default username is required",
+    }),
+  };
+  /* jscpd:ignore-end */
 
-	// Comment this out if your command does not require an org username
-	protected static requiresUsername = false;
-	// Comment this out if your command does not support a hub org username
-	protected static supportsDevhubUsername = false;
-	// Set this to true if your command requires a project workspace; 'requiresProject' is false by default
-	protected static requiresProject = true;
+  // Comment this out if your command does not require an org username
+  protected static requiresUsername = false;
+  // Comment this out if your command does not support a hub org username
+  protected static supportsDevhubUsername = false;
+  // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
+  protected static requiresProject = true;
 
-	private parser: xml2js.Parser;
-	private directories =[
-		"force-app/main/default/lwc",
-		"force-app/main/default/flows",
-		"force-app/main/default/classes",
-		"force-app/main/default/email",
-		"force-app/main/default/aura",
-		"force-app/main/default/flexipages",
-		"force-app/main/default/quickActions",
-		"force-app/main/default/objects",
-		"force-app/main/default/pages",
-		"force-app/main/default/staticresources"
-	];
-	private objectFieldsDirectory = "force-app/main/default/objects/**/fields/*.*";
-	private permissionsetsDirectory = "force-app/main/default/permissionsets/**/*.*";
-	private filePath = "force-app/main/default/labels/CustomLabels.labels-meta.xml";
+  private parser: xml2js.Parser;
+  private directories = [
+    "force-app/main/default/lwc",
+    "force-app/main/default/flows",
+    "force-app/main/default/classes",
+    "force-app/main/default/email",
+    "force-app/main/default/aura",
+    "force-app/main/default/flexipages",
+    "force-app/main/default/quickActions",
+    "force-app/main/default/objects",
+    "force-app/main/default/pages",
+    "force-app/main/default/staticresources",
+  ];
+  private objectFieldsDirectory = "force-app/main/default/objects/**/fields/*.*";
+  private permissionsetsDirectory = "force-app/main/default/permissionsets/**/*.*";
+  private filePath = "force-app/main/default/labels/CustomLabels.labels-meta.xml";
 
+  public async run(): Promise<AnyJson> {
+    // const config = await getConfig("user");
+    this.parser = new xml2js.Parser();
+    const [unusedLabels, nonReferencedFields, draftFiles] = await Promise.all([this.verifyLabels(), this.verifyFields(), this.verifyFlows()]);
+    const reports = {
+      "Unused Labels Report": unusedLabels,
+      "Non-referenced Fields Report": nonReferencedFields,
+      "Flow Draft Status Report": draftFiles,
+    };
 
-	public async run(): Promise<AnyJson> {
-		// const config = await getConfig("user");
-		this.parser = new xml2js.Parser();
-		const [unusedLabels, nonReferencedFields, draftFiles] = await Promise.all([
-			this.verifyLabels(),
-			this.verifyFields(),
-			this.verifyFlows(),
-		]);
-		const reports = {
-			"Unused Labels Report": unusedLabels,
-			"Non-referenced Fields Report": nonReferencedFields,
-			"Flow Draft Status Report": draftFiles
-		};
+    let comment = ":warning: :warning: :warning: :warning: \n\n";
+    for (const [title, items] of Object.entries(reports)) {
+      if (items.length > 0) {
+        comment += `**${title}** \n\nThe following have not been put to use:\n\n${items
+          .map((item) => `- :${this.getEmojiForReport(title)}: ${item}`)
+          .join("\n")}\n\n`;
+      }
+    }
+    if (Object.values(reports).some((items) => items.length > 0)) {
+      comment += "Please consider revisiting them.\n\n:warning: :warning: :warning: :warning:";
+    }
 
-		let comment = ':warning: :warning: :warning: :warning: \n\n';
-		for (const [title, items] of Object.entries(reports)) {
-			if (items.length > 0) {
-				comment += `**${title}** \n\nThe following have not been put to use:\n\n${items.map(item => `- :${this.getEmojiForReport(title)}: ${item}`).join('\n')}\n\n`;
-			}
-		}
-		if (Object.values(reports).some(items => items.length > 0)) {
-			comment += 'Please consider revisiting them.\n\n:warning: :warning: :warning: :warning:';
-		}
+    //TODO REMOVE just for debug
+    uxLog(this, comment);
 
-		//TODO REMOVE just for debug
-		uxLog(this,comment);
+    //TODO post comment with gitProvider class ?
 
-		//TODO post comment with gitProvider class ?
+    return {};
+  }
 
-		return {};
-	}
+  private reportEmojis = new Map<string, string>([
+    ["Unused Labels Report", "label"],
+    ["Non-referenced Fields Report", "memo"],
+    ["Flow Draft Status Report", "zap"],
+  ]);
 
-	private reportEmojis = new Map<string, string>([
-		["Unused Labels Report", "label"],
-		["Non-referenced Fields Report", "memo"],
-		["Flow Draft Status Report", "zap"],
-	]);
+  private getEmojiForReport(reportTitle: string): string {
+    return this.reportEmojis.get(reportTitle) || "label";
+  }
 
+  private async verifyFields(): Promise<string[]> {
+    const nonReferencedFields = [];
+    const ignoreNames = ["Activity"];
+    const ignoreSuffixes = ["__mdt", "__e"];
+    const fieldFiles = await glob(this.objectFieldsDirectory);
+    const filteredFieldFiles = fieldFiles.filter((file) => {
+      const objectName = file.split("/").slice(-3, -2)[0];
+      return !ignoreNames.includes(objectName) && !ignoreSuffixes.some((suffix) => objectName.endsWith(suffix));
+    });
 
-	private getEmojiForReport(reportTitle: string): string {
-		return this.reportEmojis.get(reportTitle) || "label";
-	}
+    const fieldContentsPromises = filteredFieldFiles.map((fieldFile) => fs.readFile(fieldFile, "utf-8"));
+    const fieldContents = await Promise.all(fieldContentsPromises);
+    const fieldResults = await Promise.all(fieldContents.map((content) => this.parser.parseStringPromise(content)));
 
-	private async verifyFields(): Promise<string[]> {
-		const nonReferencedFields = [];
-		const ignoreNames = ['Activity'];
-		const ignoreSuffixes = ['__mdt', '__e'];
-		const fieldFiles = await glob(this.objectFieldsDirectory);
-		const filteredFieldFiles = fieldFiles.filter(file => {
-			const objectName = file.split('/').slice(-3, -2)[0];
-			return !ignoreNames.includes(objectName) && !ignoreSuffixes.some(suffix => objectName.endsWith(suffix));
-		});
+    for (let i = 0; i < fieldResults.length; i++) {
+      const fieldResult = fieldResults[i];
+      if (fieldResult && fieldResult.CustomField && fieldResult.CustomField.fullName && fieldResult.CustomField.fullName[0].endsWith("__c")) {
+        if (fieldResult.CustomField.type) {
+          const fieldType = fieldResult.CustomField.type[0];
+          const isFieldRequired = fieldResult.CustomField.required && fieldResult.CustomField.required[0] === "true";
+          if (fieldType !== "MasterDetail" && !isFieldRequired) {
+            const fieldFile = filteredFieldFiles[i];
+            const objectName = fieldFile.split("/").slice(-3, -2)[0];
+            const fullFieldName = `${objectName}.${fieldResult.CustomField.fullName[0]}`;
+            const isFieldReferenced = await this.checkFieldPresenceInPermissionSets(fullFieldName);
+            if (!isFieldReferenced) {
+              nonReferencedFields.push(fullFieldName);
+            }
+          }
+        }
+      }
+    }
+    return nonReferencedFields;
+  }
 
-		const fieldContentsPromises = filteredFieldFiles.map(fieldFile => fs.readFile(fieldFile, 'utf-8'));
-		const fieldContents = await Promise.all(fieldContentsPromises);
-		const fieldResults = await Promise.all(fieldContents.map(content => this.parser.parseStringPromise(content)));
+  private async checkFieldPresenceInPermissionSets(fullFieldName: string): Promise<boolean> {
+    const permissionFiles = await glob(this.permissionsetsDirectory);
+    for (const file of permissionFiles) {
+      const content = await fs.readFile(file, "utf-8");
+      if (content.includes(`<field>${fullFieldName}</field>`)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-		for (let i = 0; i < fieldResults.length; i++) {
-			const fieldResult = fieldResults[i];
-			if (fieldResult && fieldResult.CustomField && fieldResult.CustomField.fullName && fieldResult.CustomField.fullName[0].endsWith("__c")) {
-				if (fieldResult.CustomField.type) {
-					const fieldType = fieldResult.CustomField.type[0];
-					const isFieldRequired = fieldResult.CustomField.required && fieldResult.CustomField.required[0] === 'true';
-					if (fieldType !== 'MasterDetail' && !isFieldRequired) {
-						const fieldFile = filteredFieldFiles[i];
-						const objectName = fieldFile.split('/').slice(-3, -2)[0];
-						const fullFieldName = `${objectName}.${fieldResult.CustomField.fullName[0]}`;
-						const isFieldReferenced = await this.checkFieldPresenceInPermissionSets(fullFieldName);
-						if (!isFieldReferenced) {
-							nonReferencedFields.push(fullFieldName);
-						}
-					}
-				}
-			}
-		}
-		return nonReferencedFields;
-	}
+  private async verifyLabels(): Promise<string[]> {
+    const unusedLabels = [];
+    fs.readFile(this.filePath, "utf-8", (errorReadingFile, data) => {
+      if (errorReadingFile) throw errorReadingFile;
 
-	private async checkFieldPresenceInPermissionSets(fullFieldName: string): Promise<boolean> {
-		const permissionFiles = await glob(this.permissionsetsDirectory);
-		for (const file of permissionFiles) {
-			const content = await fs.readFile(file, 'utf-8');
-			if (content.includes(`<field>${fullFieldName}</field>`)) {
-				return true;
-			}
-		}
-		return false;
-	}
+      xml2js.parseString(data, (errorParseString, result) => {
+        if (errorParseString) throw errorParseString;
 
-	private async verifyLabels(): Promise<string[]> {
-		const unusedLabels = [];
-		fs.readFile(this.filePath, 'utf-8', (errorReadingFile, data) => {
-			if (errorReadingFile) throw errorReadingFile;
+        const labelsArray = result.CustomLabels.labels.map((label) => label.fullName[0]);
+        const files = [];
+        this.directories.forEach((directory) => {
+          const directoryFiles = glob.sync(`${directory}/**/*.*`);
+          directoryFiles.forEach((file) => {
+            const content = fs.readFileSync(file, "utf-8").toLowerCase();
+            files.push(content);
+          });
+        });
 
-			xml2js.parseString(data, (errorParseString, result) => {
-				if (errorParseString) throw errorParseString;
+        labelsArray.forEach((label) => {
+          const labelLower = `label.${label.toLowerCase()}`;
+          const cLower = `c.${label.toLowerCase()}`;
+          const found = files.some((content) => content.includes(labelLower) || content.includes(cLower));
+          if (!found) {
+            unusedLabels.push(label);
+          }
+        });
+      });
+    });
+    return unusedLabels;
+  }
 
-				const labelsArray = result.CustomLabels.labels.map(label => label.fullName[0]);
-				const files = [];
-				this.directories.forEach(directory => {
-					const directoryFiles = glob.sync(`${directory}/**/*.*`);
-					directoryFiles.forEach(file => {
-						const content = fs.readFileSync(file, 'utf-8').toLowerCase();
-						files.push(content);
-					});
-				});
-
-				labelsArray.forEach(label => {
-					const labelLower = `label.${label.toLowerCase()}`;
-					const cLower = `c.${label.toLowerCase()}`;
-					const found = files.some(content => content.includes(labelLower) || content.includes(cLower));
-					if (!found) {
-						unusedLabels.push(label);
-					}
-				});
-			});
-		});
-		return unusedLabels;
-	}
-
-	private async verifyFlows(): Promise<string[]> {
-		const flowDirectoryFiles = await glob("force-app/main/default/flows/**/*.*");
-		const fileContentsPromises = flowDirectoryFiles.map(file => fs.readFile(file, 'utf-8'));
-		const fileContents = await Promise.all(fileContentsPromises);
-		return flowDirectoryFiles.filter((file, i) => fileContents[i].includes("<status>Draft</status>"));
-	}
+  private async verifyFlows(): Promise<string[]> {
+    const flowDirectoryFiles = await glob("force-app/main/default/flows/**/*.*");
+    const fileContentsPromises = flowDirectoryFiles.map((file) => fs.readFile(file, "utf-8"));
+    const fileContents = await Promise.all(fileContentsPromises);
+    return flowDirectoryFiles.filter((file, i) => fileContents[i].includes("<status>Draft</status>"));
+  }
 }
