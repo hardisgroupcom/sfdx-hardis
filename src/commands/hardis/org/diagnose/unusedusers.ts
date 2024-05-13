04dfc110278fe40762117a62e71ae469e5c0cc17
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2024-05-13 22:35:50 +0200

    Revert "Slight refactoring ^^"
    This reverts commit 1adbe045dfee33a6154e6e25c1324ff28744e44a.

--- src/commands/hardis/org/diagnose/unusedusers.ts
+++ src/commands/hardis/org/diagnose/unusedusers.ts
@@ -4,7 +4,7 @@ import { Messages } from "@salesforce/core";
 import { AnyJson } from "@salesforce/ts-types";
 import * as c from "chalk";
 import { isCI, uxLog } from "../../../../common/utils";
-import { bulkQuery } from "../../../../common/utils/apiUtils";
+import { bulkQuery} from "../../../../common/utils/apiUtils";
 import { generateCsvFile, generateReportPath } from "../../../../common/utils/filesUtils";
 import { NotifProvider, NotifSeverity } from "../../../../common/notifProvider";
 import { getNotificationButtons, getOrgMarkdown } from "../../../../common/utils/notifUtils";
@@ -22,10 +22,7 @@ export default class DiagnoseUnusedUsers extends SfdxCommand {
 
   public static description = `Efficient user management is vital in Salesforce to ensure resources are optimized and costs are controlled. However, inactive or unused user accounts can often go unnoticed, leading to wasted licenses and potential security risks. This tool addresses this challenge by enabling administrators to identify users who haven't logged in within a specified period.
 
-By analyzing user login activity and last login timestamps, this feature highlights inactive user accounts, allowing administrators to take appropriate action. Whether it's deactivating dormant accounts, freeing up licenses, or ensuring compliance with security policies, this functionality empowers administrators to maintain a lean and secure Salesforce environment.
-  
-You can see the full list of available license identifiers in [Salesforce Documentation](https://developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_userlicense.htm)
-`;
+  By analyzing user login activity and last login timestamps, this feature highlights inactive user accounts, allowing administrators to take appropriate action. Whether it's deactivating dormant accounts, freeing up licenses, or ensuring compliance with security policies, this functionality empowers administrators to maintain a lean and secure Salesforce environment.`;
 
 
   public static examples = ["$ sfdx hardis:org:diagnose:unusedusers", "$ sfdx hardis:org:diagnose:unusedusers --days 365"];
@@ -41,14 +38,10 @@ You can see the full list of available license identifiers in [Salesforce Docume
       default: 100,
       description: "Extracts the users that have been inactive for the amount of days specified.",
     }),
-    licensetypes: flags.enum({
-      char: "l",
-      options: ["all", "all-crm", "all-paying"],
-      description: "Type of licenses to check. If set, do not use licenseidentifiers option",
-    }),
-    licenseidentifiers: flags.string({
-      char: "i",
-      description: "Comma-separated list of license identifiers, in case licensetypes is not used.. Identifiers available at https://developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_userlicense.htm",
+    licensetypes: flags.string({
+      char: "t",
+      default: 'SFDC,AUL,AUL1,AULL_IGHT',
+      description: "Extracts the users that have the following license types.",
     }),
     debug: flags.boolean({
       char: "d",
@@ -69,20 +62,11 @@ You can see the full list of available license identifiers in [Salesforce Docume
   protected static requiresDevhubUsername = false;
   // Set this to true if your command requires a project workspace; 'requiresProject' is false by default
   protected static requiresProject = false;
-
-  protected licenseTypesCorrespondances = {
-    'all': 'all',
-    'all-crm': 'SFDC,AUL,AUL1,AULL_IGHT',
-    'all-paying': 'SFDC,AUL,AUL1,AULL_IGHT,PID_Customer_Community,PID_Customer_Community_Login,PID_Partner_Community,PID_Partner_Community_Login'
-  }
-
-
   protected debugMode = false;
   protected outputFile;
   protected outputFilesRes: any = {};
   protected lastNdays: number;
   protected licenseTypes: string;
-  protected licenseIdentifiers: string;
   protected unusedUsers = [];
   protected statusCode = 0;
 
@@ -91,18 +75,86 @@ You can see the full list of available license identifiers in [Salesforce Docume
   public async run(): Promise<AnyJson> {
     this.debugMode = this.flags.debug || false;
     this.outputFile = this.flags.outputfile || null;
-    this.lastNdays = this.flags.days || null;
-    this.licenseIdentifiers = this.flags.licenseidentifiers || null;
+    this.lastNdays = this.flags.days;
     this.licenseTypes = this.flags.licensetypes;
 
-    // Calculate lastNdays to use
-    await this.defineNumberOfInactiveDays();
-    // Calculate license identifiers from input parameters or default values
-    await this.defineLicenseIdentifiers();
+    // If manual mode and days not sent as parameter, prompt user
+    if (!isCI) {
+      if(!this.lastNdays){
+        const lastNdaysResponse = await prompts({
+          type: "select",
+          name: "days",
+          message: "Please select the period to detect inactive active users.",
+          choices: [
+            { title: `7 days`, value: 7 },
+            { title: `30 days`, value: 30 },
+            { title: `90 days`, value: 90 },
+            { title: `6 months (180 days)`, value: 180 },
+            { title: `1 year (365 days)`, value: 365 },
+            { title: `2 years (730 days)`, value: 730 }
+          ],
+        });
+        this.lastNdays = lastNdaysResponse.days;
+      }
+      
 
-    // Retrieve the list of users who haven't logged in for a while
+      if(!this.licenseTypes){
+        const licenseTypesResponse = await prompts({
+          type: "select",
+          name: "licensetypes",
+          message: "Please select the period to detect inactive active users.",
+          choices: [
+            { title: 'all', value: 'all' },
+            { title: `all-crm`, value: 'SFDC,AUL,AUL1,AULL_IGHT' },
+            { title: `all-paying`, value: 'SFDC,AUL,AUL1,AULL_IGHT,PID_Customer_Community,PID_Customer_Community_Login,PID_Partner_Community,PID_Partner_Community_Login' },
+            // { title: 'all-original', value: 'AUL, AUL1, AUL_LIGHT, FDC_ONE, FDC_SUB, Overage_Platform_Portal_User, PID_STRATEGIC_PRM, PID_CHATTER, PID_CONTENT, PID_Customer_Portal_Basic, PID_Customer_Portal_Standard, PID_FDC_FREE, PID_IDEAS, PID_Ideas_Only_Portal, PID_Ideas_Only_Site, PID_KNOWLEDGE, PID_Customer_Community, PID_Customer_Community_Login, PID_Partner_Community, PID_Partner_Community_Login, PID_Limited_Customer_Portal_Basic, PID_Limited_Customer_Portal_Standard, PID_Overage_Customer_Portal_Basic, PID_Overage_High Volume Customer Portal, Platform_Portal_User, POWER_PRM, POWER_SSP, SFDC'}
+            // I think you can add more licenses types by default, to handle all the "paying" ones
+
+            // For example Platform & Partner Community licenses ^^
+            
+            // https://developer.salesforce.com/docs/atlas.en-us.object_reference.meta/object_reference/sforce_api_objects_userlicense.htm
+            
+            // Maybe add a parameter licensetypes that could be:
+            
+            // all-crm that would be converted into SFDC,AUL,AUL1,AULL_IGHT... (default value)
+            // all-paying that would be all-crm + community types licenses
+            // all ( no filter on license type)
+            // a list of licence type Ids
+            // prompted if not specified and not in CI
+          ],
+        });
+        this.licenseTypes = licenseTypesResponse.licensetypes;
+      }
+    }
+
+    // If manual mode and lastndays not sent as parameter, prompt user
+    if (!isCI && !this.licenseTypes) {
+      const lastNdaysResponse = await prompts({
+        type: "select",
+        name: "lastndays",
+        message: "Please select the number of days in the past from today you want to detect suspiscious setup activities",
+        choices: [
+          { title: `1`, value: 1 },
+          { title: `2`, value: 2 },
+          { title: `3`, value: 3 },
+          { title: `4`, value: 4 },
+          { title: `5`, value: 5 },
+          { title: `6`, value: 6 },
+          { title: `7`, value: 7 },
+          { title: `14`, value: 14 },
+          { title: `30`, value: 30 },
+          { title: `60`, value: 60 },
+          { title: `90`, value: 90 },
+          { title: `180`, value: 180 },
+        ],
+      });
+      this.lastNdays = lastNdaysResponse.lastndays;
+    }
+
     const conn = this.org.getConnection();
     uxLog(this, c.cyan(`Extracting active users who haven't logged in for a while on ${conn.instanceUrl} ...`));
+
+    // Retrieve the list of users who haven't logged in for a while
     this.unusedUsers = await this.listUnusedUsersWithSfdcLicense(conn);
 
     // Generate output CSV file
@@ -112,11 +164,13 @@ You can see the full list of available license identifiers in [Salesforce Docume
     }
 
     const userSummaryInfo = this.unusedUsers.length == 1 ? 'user has' : 'users have'
-    let summary = `No unused users have been found`;
-    if (this.unusedUsers.length === 0) {
+    let msg = `No unused users have been found`;
+    let summary;
+    if(this.unusedUsers.length == 0){
       summary = `All users have logged in to ${conn.instanceUrl} within the last ${this.lastNdays} days!`;
     } else {
       this.statusCode = 1;
+      msg = `${this.unusedUsers.length} unused ${userSummaryInfo} been found`;
       summary = `${this.unusedUsers.length} active ${userSummaryInfo} not logged in to ${conn.instanceUrl} in the last ${this.lastNdays} days!`;
     }
 
@@ -125,19 +179,12 @@ You can see the full list of available license identifiers in [Salesforce Docume
     }
 
     // Manage notifications
-    await this.manageNotifications(this.unusedUsers);
-
-    if (this.unusedUsers.length > 0) {
-      uxLog(this, c.yellow(summary));
-      console.table(this.unusedUsers);
-    }
-    else {
-      uxLog(this, c.green(summary));
-    }
+    await this.manageNotifications(this.unusedUsers, summary);
 
     // Return an object to be displayed with --json
     return {
       status: this.statusCode,
+      message: msg,
       summary: summary,
       unusedUsers: this.unusedUsers,
       csvLogFile: this.outputFile,
@@ -145,78 +192,30 @@ You can see the full list of available license identifiers in [Salesforce Docume
     };
   }
 
-  private async defineLicenseIdentifiers() {
-    if (!this.licenseIdentifiers) {
-      // Ask user if interactive mode
-      if (!this.licenseTypes && !isCI) {
-        const licenseTypesResponse = await prompts({
-          type: "select",
-          name: "licensetypes",
-          message: "Please select the period to detect inactive active users.",
-          choices: [
-            { title: 'all', value: 'all' },
-            { title: `all-crm`, value: 'SFDC,AUL,AUL1,AULL_IGHT' },
-            { title: `all-paying`, value: 'SFDC,AUL,AUL1,AULL_IGHT,PID_Customer_Community,PID_Customer_Community_Login,PID_Partner_Community,PID_Partner_Community_Login' },
-          ],
-        });
-        this.licenseTypes = licenseTypesResponse.licensetypes;
-      }
-      else {
-        this.licenseTypes = "all-crm";
-      }
-      // Get licenseIdentifiers from licenseType
-      this.licenseIdentifiers = this.licenseTypesCorrespondances[this.licenseTypes];
-    }
-  }
-
-  private async defineNumberOfInactiveDays() {
-    if (!this.lastNdays) {
-      if (!isCI) {
-        // If manual mode and days not sent as parameter, prompt user
-        const lastNdaysResponse = await prompts({
-          type: "select",
-          name: "days",
-          message: "Please select the period to detect inactive active users.",
-          choices: [
-            { title: `7 days`, value: 7 },
-            { title: `30 days`, value: 30 },
-            { title: `60 days`, value: 60 },
-            { title: `3 months (90 days)`, value: 90 },
-            { title: `6 months (180 days)`, value: 180 },
-            { title: `1 year (365 days)`, value: 365 },
-            { title: `2 years (730 days)`, value: 730 }
-          ],
-        });
-        this.lastNdays = lastNdaysResponse.days;
-      }
-      else {
-        this.lastNdays = 90;
-      }
-    }
-  }
-
   private async listUnusedUsersWithSfdcLicense(conn) {
-    let whereConstraint = `WHERE IsActive = true AND (` +
-      `(LastLoginDate != LAST_N_DAYS:${this.lastNdays} AND LastLoginDate != NULL) OR ` +
-      `(CreatedDate != LAST_N_DAYS:${this.lastNdays} AND LastLoginDate == NULL)` + // Check also for users never used
-      `)`;
-    // Add License constraint only if necessary
+    let whereConstraint = `WHERE IsActive = true AND LastLoginDate != LAST_N_DAYS:${this.lastNdays} AND LastLoginDate != NULL`;
+    
     if (this.licenseTypes !== 'all') {
       const licenseTypeValues = this.licenseTypes.split(',');
       const licenseTypeCondition = licenseTypeValues.map(value => `'${value}'`).join(',');
       whereConstraint += ` AND Profile.UserLicense.LicenseDefinitionKey IN (${licenseTypeCondition})`;
     }
-    // Build & call Bulk API Query
+  
     const unusedUsersQuery =
       `SELECT Id, User.Firstname, User.LastName, Profile.Name, Username, LastLoginDate, IsActive, Profile.UserLicense.LicenseDefinitionKey ` +
       `FROM User ` +
       whereConstraint +
       ` ORDER BY LastLoginDate DESC`;
+    
+    console.log(unusedUsersQuery);
+    
+    uxLog(this, c.grey("Query: " + c.italic(unusedUsersQuery)));
     const unusedUsersQueryRes = await bulkQuery(unusedUsersQuery, conn);
+  
     return unusedUsersQueryRes.records;
   }
 
-  private async manageNotifications(unusedUsers: any[]) {
+  private async manageNotifications(unusedUsers: any[], summary: any) {
     // Build notification
     const orgMarkdown = await getOrgMarkdown(this.org?.getConnection()?.instanceUrl);
     const notifButtons = await getNotificationButtons();
@@ -227,6 +226,10 @@ You can see the full list of available license identifiers in [Salesforce Docume
     if (unusedUsers.length > 0) {
       notifSeverity = "warning";
       notifText = `${this.unusedUsers.length} active users have not logged in to ${orgMarkdown} within the last ${this.lastNdays} days.`;
+      // for (const pslMasterLabel of Object.keys(summary).sort()) {
+      //   const psl = this.getPermissionSetLicenseByMasterLabel(pslMasterLabel);
+      //   notifDetailText += `• ${pslMasterLabel}: ${summary[pslMasterLabel]} (${psl.UsedLicenses} used on ${psl.TotalLicenses} available)\n`;
+      // }
       attachments = [{ text: notifDetailText }];
     }
     // Send notifications
@@ -246,4 +249,12 @@ You can see the full list of available license identifiers in [Salesforce Docume
     });
     return [];
   }
+
+  // private getPermissionSetLicenseByMasterLabel(masterLabel: string) {
+  //   const pslList = this.permissionSetLicenses.filter((psl) => psl.MasterLabel === masterLabel);
+  //   if (pslList.length === 1) {
+  //     return pslList[0];
+  //   }
+  //   throw new SfdxError(`Unable to find Permission Set License with MasterLabel ${masterLabel}`);
+  // }
 }
