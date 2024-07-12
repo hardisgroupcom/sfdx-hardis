437427bd532945f123aaba73e9a55c3650d2af65
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2024-07-12 16:49:22 +0200

    GITLAB_API_REJECT_UNAUTHORIZED (#644)
    * GITLAB_API_REJECT_UNAUTHORIZED
    
    GITLAB_API_REJECT_UNAUTHORIZED: set to `"false"` if you want to allow connection even without certificate (can be useful on on-premise GitLab instance)
    
    * [Mega-Linter] Apply linters fixes
    
    ---------
    
    Co-authored-by: nvuillam <nvuillam@users.noreply.github.com>

--- src/common/gitProvider/gitlab.ts
+++ src/common/gitProvider/gitlab.ts
@@ -13,7 +13,11 @@ export class GitlabProvider extends GitProviderRoot {
     this.serverUrl = process.env.CI_SERVER_URL;
     // It's better to have a project token defined in a CI_SFDX_HARDIS_GITLAB_TOKEN variable, to have the rights to act on Pull Requests
     this.token = process.env.CI_SFDX_HARDIS_GITLAB_TOKEN || process.env.ACCESS_TOKEN;
-    this.gitlabApi = new Gitlab({ host: this.serverUrl, token: this.token });
+    this.gitlabApi = new Gitlab({
+      host: this.serverUrl,
+      token: this.token,
+      rejectUnauthorized: process?.env?.GITLAB_API_REJECT_UNAUTHORIZED === "false" ? false : true,
+    });
   }
 
   public getLabel(): string {
