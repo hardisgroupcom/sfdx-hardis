ed83de9915f99b44bcde6fe5f4202cc39afd54f7
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2024-08-29 17:54:31 +0200

    sss

--- src/hooks/auth/auth.ts
+++ src/hooks/auth/auth.ts
@@ -1,52 +1,17 @@
-import c from 'chalk';
 import {
-  elapseStart,
   getCurrentGitBranch,
   isCI,
-  restoreLocalSfdxInfo,
-  uxLog,
 } from '../../common/utils/index.js';
 import { checkConfig, getConfig } from '../../config/index.js';
 import { Hook } from '@oclif/core';
 import { authOrg } from '../../common/utils/authUtils.js';
 
-const hook: Hook<'prerun'> = async (options) => {
-  // Skip hooks from other commands than hardis commands
+const hook: Hook<'auth'> = async (options: any) => {
   const commandId = options?.Command?.id || '';
-
-  if (commandId.startsWith('hardis')) {
-    elapseStart(`${options?.Command?.id} execution time`);
-  }
-
-  if (
-    !commandId.startsWith('hardis') ||
-    [
-      'hardis:doc:plugin:generate',
-      'hardis:source:push',
-      'hardis:source:pull',
-      'hardis:scratch:pool:view',
-      'hardis:source:deploy',
-      'hardis:source:push',
-      'hardis:mdapi:deploy',
-    ].includes(commandId)
-  ) {
-    return;
-  }
-  // skip if during mocha tests
-  if (typeof global.it === 'function') {
-    return;
-  }
-  await restoreLocalSfdxInfo();
   let configInfo = await getConfig('user');
-  if (configInfo.skipAuthCheck === true) {
-    uxLog(this, c.yellow('No authentication check, you better know what you are doing ;)'));
-    return;
-  }
+
   // Manage authentication if DevHub is required but current user is disconnected
-  if (
-    (options.Command && (options?.Command?.flags as any)['target-dev-hub']?.required === true) ||
-    (options as any)?.devHub === true
-  ) {
+  if ((options as any)?.devHub === true) {
     let devHubAlias = configInfo.devHubAlias || process.env.DEVHUB_ALIAS;
     if (devHubAlias == null) {
       await checkConfig(options);
@@ -57,8 +22,7 @@ const hook: Hook<'prerun'> = async (options) => {
   }
   // Manage authentication if org is required but current user is disconnected
   if (
-    (((options?.Command?.flags as any)['target-org']?.required === true && !options?.argv?.includes('--skipauth')) ||
-      (options as any)?.checkAuth === true) &&
+    (options as any)?.checkAuth === true &&
     !((options as any)?.devHub === true)
   ) {
     const orgAlias = (options as any)?.alias
@@ -77,7 +41,5 @@ const hook: Hook<'prerun'> = async (options) => {
     await authOrg(orgAlias, options);
   }
 };
-
-
 
 export default hook;
