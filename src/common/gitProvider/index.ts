8f7f3d2e3ff41065e1dbbe951bbab42b8cce5880
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2025-01-01 16:15:30 +0100

    Mermaid in PNG for Azure & Bitbucket (#964)
    * typo
    
    * Convert SVG to PNG on Azure & Bitbucket
    
    * Handle PNG generation for Azure & Bitbucket
    
    * typo

--- src/common/gitProvider/index.ts
+++ src/common/gitProvider/index.ts
@@ -193,6 +193,14 @@ export abstract class GitProvider {
     return gitProvider.supportsMermaidInPrMarkdown();
   }
 
+  static async supportsSvgAttachments(): Promise<boolean> {
+    const gitProvider = await GitProvider.getInstance();
+    if (gitProvider == null) {
+      return false;
+    }
+    return gitProvider.supportsSvgAttachments();
+  }
+
   static async getPullRequestInfo(): Promise<any> {
     const gitProvider = await GitProvider.getInstance();
     if (gitProvider == null) {
