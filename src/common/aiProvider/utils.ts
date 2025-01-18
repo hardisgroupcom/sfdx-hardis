690e7a46de15111d97bde2ce7ce73234db5df811
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2025-01-18 22:25:26 +0100

    Handle prompts multilinguism and cache (#1015)
    * Handle prompts multilinguism and cache
    
    * [Mega-Linter] Apply linters fixes :)
    
    ---------
    
    Co-authored-by: nvuillam <17500430+nvuillam@users.noreply.github.com>

--- src/common/aiProvider/utils.ts
+++ src/common/aiProvider/utils.ts
@@ -1,4 +1,9 @@
 import { getEnvVar } from "../../config/index.js";
+import { PromptTemplate } from "./promptTemplates.js";
+import path from 'path';
+import fs from 'fs-extra';
+import { XMLParser } from "fast-xml-parser";
+import farmhash from 'farmhash';
 
 export class UtilsAi {
   public static isOpenAiAvailable() {
@@ -7,4 +12,49 @@ export class UtilsAi {
     }
     return false;
   }
+
+  public static getPromptsLanguage(): string {
+    return process.env.PROMPTS_LANGUAGE || "en";
+  }
+
+  public static async findAiCache(template: PromptTemplate, promptParameters: any[]): Promise<{ success: boolean, cacheText?: string, fingerPrint: string, flowsAiCacheDirFile: string }> {
+    const fingerPrint = this.getFingerPrint(promptParameters);
+    const lang = this.getPromptsLanguage();
+    const flowsAiCacheDirFile = path.join("docs", "cache-ai-results", `${lang}-${template}-${fingerPrint}.md`);
+    if (process.env?.IGNORE_AI_CACHE === "true") {
+      return { success: false, fingerPrint, flowsAiCacheDirFile };
+    }
+    if (fs.existsSync(flowsAiCacheDirFile)) {
+      const cacheText = await fs.readFile(flowsAiCacheDirFile, "utf8");
+      return { success: true, cacheText, fingerPrint, flowsAiCacheDirFile };
+    }
+    return { success: false, fingerPrint, flowsAiCacheDirFile };
+  }
+
+  public static async writeAiCache(template: PromptTemplate, promptParameters: any[], aiCacheText: string): Promise<void> {
+    const fingerPrint = this.getFingerPrint(promptParameters);
+    const flowsAiCacheDir = path.join("docs", "cache-ai-results");
+    await fs.ensureDir(flowsAiCacheDir);
+    const lang = this.getPromptsLanguage();
+    const flowsAiCacheDirFile = path.join(flowsAiCacheDir, `${lang}-${template}-${fingerPrint}.md`);
+    await fs.writeFile(flowsAiCacheDirFile, aiCacheText);
+  }
+
+  public static getFingerPrint(promptParameters: any[]): string {
+    const parametersFingerPrints = promptParameters.map((promptParameter) => {
+      if (typeof promptParameter === "string" && promptParameter.includes("<xml")) {
+        try {
+          const xmlObj = new XMLParser().parse(promptParameter);
+          return farmhash.fingerprint32(JSON.stringify(xmlObj));
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        catch (e) {
+          return farmhash.fingerprint32(promptParameter);
+        }
+      }
+      return farmhash.fingerprint32(JSON.stringify(promptParameter));
+    });
+    return parametersFingerPrints.join("-");
+  }
+
 }
--- src/common/utils/mermaidUtils.ts
+++ src/common/utils/mermaidUtils.ts
@@ -10,8 +10,7 @@ import moment from "moment";
 import { SfError } from "@salesforce/core";
 import { PACKAGE_ROOT_DIR } from "../../settings.js";
 import { AiProvider } from "../aiProvider/index.js";
-import { XMLParser } from "fast-xml-parser";
-import farmhash from "farmhash";
+import { UtilsAi } from "../aiProvider/utils.js";
 
 let IS_MERMAID_AVAILABLE: boolean | null = null;
 export async function isMermaidAvailable() {
@@ -670,10 +669,11 @@ export function removeMermaidLinks(messageBody: string) {
 }
 
 async function completeWithAiDescription(flowMarkdownDoc: string, flowXml: string): Promise<string> {
-  const aiCache = await findAiCache(flowXml);
-  if (aiCache) {
+  const aiCache = await UtilsAi.findAiCache("PROMPT_DESCRIBE_FLOW", [flowXml]);
+  if (aiCache.success === true) {
     uxLog(this, c.grey("Used AI cache for flow description (set IGNORE_AI_CACHE=true to force call to AI)"));
-    return flowMarkdownDoc.replace("<!-- Flow description -->", aiCache);
+    const replaceText = `## AI-Generated Description\n\n<!-- Cache file: ${aiCache.flowsAiCacheDirFile} -->\n\n${aiCache.cacheText || ""}`;
+    return flowMarkdownDoc.replace("<!-- Flow description -->", replaceText);
   }
   if (AiProvider.isAiAvailable()) {
     // Invoke AI Service
@@ -685,9 +685,9 @@ async function completeWithAiDescription(flowMarkdownDoc: string, flowXml: strin
       if (responseText.startsWith("##")) {
         responseText = responseText.split("\n").slice(1).join("\n");
       }
-      const replaceText = "## AI-Generated Description\n\n" + responseText;
+      await UtilsAi.writeAiCache("PROMPT_DESCRIBE_FLOW", [flowXml], responseText);
+      const replaceText = `## AI-Generated Description\n\n<!-- Cache file: ${aiCache.flowsAiCacheDirFile} -->\n\n${responseText}`;
       const flowMarkdownDocUpdated = flowMarkdownDoc.replace("<!-- Flow description -->", replaceText);
-      await writeAiCache(flowXml, "", replaceText);
       return flowMarkdownDocUpdated;
     }
   }
@@ -696,10 +696,11 @@ async function completeWithAiDescription(flowMarkdownDoc: string, flowXml: strin
 
 /* jscpd:ignore-start */
 async function completeWithDiffAiDescription(flowMarkdownDoc: string, flowXmlNew: string, flowXmlPrevious: string): Promise<string> {
-  const aiCache = await findAiCache(flowXmlNew, flowXmlPrevious);
-  if (aiCache) {
+  const aiCache = await UtilsAi.findAiCache("PROMPT_DESCRIBE_FLOW_DIFF", [flowXmlNew, flowXmlPrevious]);
+  if (aiCache.success) {
     uxLog(this, c.grey("Used AI cache for diff description (set IGNORE_AI_CACHE=true to force call to AI)"));
-    return flowMarkdownDoc.replace("<!-- Flow description -->", aiCache);
+    const replaceText = `## AI-Generated Differences Summary\n\n<!-- Cache file: ${aiCache.flowsAiCacheDirFile} -->\n\n${aiCache.cacheText || ""}`;
+    return flowMarkdownDoc.replace("<!-- Flow description -->", replaceText);
   }
   if (AiProvider.isAiAvailable()) {
     // Invoke AI Service
@@ -711,43 +712,13 @@ async function completeWithDiffAiDescription(flowMarkdownDoc: string, flowXmlNew
       if (responseText.startsWith("##")) {
         responseText = responseText.split("\n").slice(1).join("\n");
       }
-      const replaceText = "## AI-Generated Differences Summary\n\n" + responseText;
+      await UtilsAi.writeAiCache("PROMPT_DESCRIBE_FLOW_DIFF", [flowXmlNew, flowXmlPrevious], responseText);
+      const replaceText = `## AI-Generated Differences Summary\n\n<!-- Cache file: ${aiCache.flowsAiCacheDirFile} -->\n\n${aiCache.cacheText || ""}`;
       const flowMarkdownDocUpdated = flowMarkdownDoc.replace("<!-- Flow description -->", replaceText);
-      await writeAiCache(flowXmlNew, flowXmlPrevious, replaceText);
       return flowMarkdownDocUpdated;
     }
   }
   return flowMarkdownDoc;
 }
 /* jscpd:ignore-end */
-
-async function findAiCache(flowXml1: string, flowXml2: string = ""): Promise<string | null> {
-  if (process.env?.IGNORE_AI_CACHE === "true") {
-    return null;
-  }
-  const fingerPrint = getFingerPrint(flowXml1, flowXml2);
-  const flowsAiCacheDirFile = path.join("docs", "flows", "ai-results", fingerPrint + ".md");
-  if (fs.existsSync(flowsAiCacheDirFile)) {
-    return await fs.readFile(flowsAiCacheDirFile, "utf8");
-  }
-  return null;
-}
-
-async function writeAiCache(flowXml1: string, flowXml2: string, aiCacheText: string): Promise<void> {
-  const fingerPrint = getFingerPrint(flowXml1, flowXml2);
-  const flowsAiCacheDir = path.join("docs", "flows", "ai-results");
-  await fs.ensureDir(flowsAiCacheDir);
-  const flowsAiCacheDirFile = path.join(flowsAiCacheDir, fingerPrint + ".md");
-  await fs.writeFile(flowsAiCacheDirFile, aiCacheText);
-}
-
-function getFingerPrint(flowXml1: string, flowXml2: string) {
-  const flowObj = new XMLParser().parse(flowXml1)?.Flow || "";
-  let fingerPrint = String(farmhash.fingerprint32(JSON.stringify(flowObj)));
-  if (flowXml2 !== "") {
-    const flowObj2 = new XMLParser().parse(flowXml2)?.Flow || "";
-    fingerPrint += "-" + String(farmhash.fingerprint32(JSON.stringify(flowObj2)));
-  }
-  return fingerPrint;
-}
 
