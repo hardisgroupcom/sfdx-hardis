17aff1ef4db5e42f8cd2ac2e45e3b184d3d3572e
Mariia Pyvovarchuk <pyvovarchukmaria@gmail.com>
2025-05-04 22:07:52 +0300

    Feature: introducing Assignment Rules documentation + AI description (#1186)
    * Implemented doc builder to generate documentation for Assignment Rules.
    
    * Updated prompts with request for describing Assignment Rules.
    
    * Updated project2markdown file with logic for generating docs for Assignment Rules.
    
    * Removed comments.
    
    * Updated cspell script and changelog.
    
    * Updated cspell script.
    
    * Added one single env var for Approval Processes and Assignment Rules to disable docs generation.
    
    * Updated prompt for Assignment Rules.
    
    * Added comments.
    
    * Updated comments.
    
    * Updated docs view: added more details for rule entries.
    
    * Moved logic that will be reusable for other rules into a separate util class.
    
    * Updated prompt templates
    
    * UPD: refactored utils for building rules.
    
    * Updated changelog: fixed merge conflict.
    
    * UPD prefer-const error fixed.
    
    * Update doc
    
    * changelog
    
    * change order
    
    * Refactoring: parsed one xml doc into separate mkd pages for each Assignment Rule.
    
    ---------
    
    Co-authored-by: Nicolas Vuillamy <nicolas.vuillamy@gmail.com>

--- src/commands/hardis/doc/project2markdown.ts
+++ src/commands/hardis/doc/project2markdown.ts
@@ -4,7 +4,7 @@ import fs from 'fs-extra';
 import c from "chalk";
 import * as path from "path";
 import { process as ApexDocGen } from '@cparra/apexdocs';
-import { XMLParser } from "fast-xml-parser";
+import {XMLBuilder, XMLParser} from "fast-xml-parser";
 import sortArray from 'sort-array';
 import { Messages } from '@salesforce/core';
 import { AnyJson } from '@salesforce/ts-types';
@@ -31,6 +31,7 @@ import { DocBuilderFlow } from '../../../common/docBuilder/docBuilderFlow.js';
 import { DocBuilderPackageXML } from '../../../common/docBuilder/docBuilderPackageXml.js';
 import { DocBuilderPermissionSet } from '../../../common/docBuilder/docBuilderPermissionSet.js';
 import { DocBuilderPermissionSetGroup } from '../../../common/docBuilder/docBuilderPermissionSetGroup.js';
+import { DocBuilderAssignmentRules } from '../../../common/docBuilder/docBuilderAssignmentRules.js';
 import { DocBuilderApprovalProcess } from '../../../common/docBuilder/docBuilderApprovalProcess.js';
 
 Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
@@ -160,6 +161,7 @@ ${this.htmlInstructions}
   protected profileDescriptions: any[] = [];
   protected permissionSetsDescriptions: any[] = [];
   protected permissionSetGroupsDescriptions: any[] = [];
+  protected assignmentRulesDescriptions: any[] = [];
   protected approvalProcessesDescriptions: any[] = [];
   protected objectDescriptions: any[] = [];
   protected objectFiles: string[];
@@ -186,6 +188,7 @@ ${this.htmlInstructions}
       "- [Objects](objects/index.md)",
       "- Automations",
       "  - [Approval Processes](approvalProcesses/index.md)",
+      "  - [Assignment Rules](assignmentRules/index.md)",
       "  - [Flows](flows/index.md)",
       "- Authorizations",
       "  - [Profiles](profiles/index.md)",
@@ -271,9 +274,11 @@ ${this.htmlInstructions}
       await this.generateObjectsDocumentation();
     }
 
-    // List approval processes & generate doc
-    if (!(process?.env?.GENERATE_APPROVAL_PROCESS_DOC === 'false')) {
+    if (!(process?.env?.GENERATE_AUTOMATIONS_DOC === 'false')) {
+      // List approval processes & generate doc
       await this.generateApprovalProcessDocumentation();
+      // List assignment rules and generate doc
+      await this.generateAssignmentRulesDocumentation();
     }
 
     // Write output index file
@@ -504,9 +509,61 @@ ${Project2Markdown.htmlInstructions}
     await fs.writeFile(psgIndexFile, getMetaHideLines() + DocBuilderPermissionSetGroup.buildIndexTable('', this.permissionSetGroupsDescriptions).join("\n") + `\n${this.footer}\n`);
   }
 
+  private async generateAssignmentRulesDocumentation() {
+    uxLog(this, c.cyan("Generating Assignment Rules documentation... " +
+      "(if you don't want it, define GENERATE_AUTOMATIONS_DOC=false in your environment variables)"));
+
+    const assignmentRulesForMenu: any = {"All Assignment Rules": "assignmentRules/index.md"};
+    const assignmentRulesFiles = (await glob("**/assignmentRules/**.assignmentRules-meta.xml", {
+      cwd: process.cwd(),
+      ignore: GLOB_IGNORE_PATTERNS
+    })).sort();
+    const builder = new XMLBuilder();
+
+    for (const assignmentRulesFile of assignmentRulesFiles) {
+
+      const assignmentRulesXml = await fs.readFile(assignmentRulesFile, "utf8");
+      const assignmentRulesXmlParsed = new XMLParser().parse(assignmentRulesXml);
+
+      const assignmentRulesName = path.basename(assignmentRulesFile, ".assignmentRules-meta.xml");
+      assignmentRulesForMenu[assignmentRulesName] = "assignmentRules/" + assignmentRulesName + ".md";
+
+      // parsing one singe XML file with all the Assignment Rules per object:
+      let rulesList = assignmentRulesXmlParsed?.AssignmentRules?.assignmentRule || [];
+      if (!Array.isArray(rulesList)) {
+        rulesList = [rulesList];
+      }
+
+      for (const rule of rulesList) {
+        const currentRuleName = assignmentRulesName + "." + rule?.fullName;
+        assignmentRulesForMenu[currentRuleName] = "assignmentRules/" + currentRuleName + ".md";
+        const mdFile = path.join(this.outputMarkdownRoot, "assignmentRules", currentRuleName + ".md");
+
+        this.assignmentRulesDescriptions.push({
+          name: currentRuleName,
+          active: rule.active,
+        });
+
+        const ruleXml = builder.build({ assignmentRule: rule });
+
+        await new DocBuilderAssignmentRules(currentRuleName, ruleXml, mdFile).generateMarkdownFileFromXml();
+        if (this.withPdf) {
+          await generatePdfFileFromMarkdown(mdFile);
+        }
+      }
+    }
+
+    this.addNavNode("Assignment Rules", assignmentRulesForMenu);
+
+    // Write index file for permission set groups folder
+    await fs.ensureDir(path.join(this.outputMarkdownRoot, "assignmentRules"));
+    const psgIndexFile = path.join(this.outputMarkdownRoot, "assignmentRules", "index.md");
+    await fs.writeFile(psgIndexFile, getMetaHideLines() + DocBuilderAssignmentRules.buildIndexTable('', this.assignmentRulesDescriptions).join("\n") + `\n${this.footer}\n`);
+  }
+
   private async generateApprovalProcessDocumentation() {
     uxLog(this, c.cyan("Generating Approval Processes documentation... " +
-      "(if you don't want it, define GENERATE_APPROVAL_PROCESS_DOC=false in your environment variables)"));
+      "(if you don't want it, define GENERATE_AUTOMATIONS_DOC=false in your environment variables)"));
 
     const approvalProcessesForMenu: any = { "All Approval Processes": "approvalProcesses/index.md" }
     const approvalProcessFiles = (await glob("**/approvalProcesses/**.approvalProcess-meta.xml", {
@@ -628,7 +685,7 @@ ${Project2Markdown.htmlInstructions}
 
     // Add root menus
     const rootSections = [
-      { menu: "Automations", subMenus: ["Approval Processes", "Flows"] },
+      { menu: "Automations", subMenus: ["Approval Processes", "Assignment Rules", "Flows"] },
       { menu: "Authorizations", subMenus: ["Profiles", "Permission Set Groups", "Permission Sets"] },
       { menu: "Code", subMenus: ["Apex"] },
     ];
@@ -739,6 +796,9 @@ ${Project2Markdown.htmlInstructions}
       // Add Approval Processes table
       const relatedApprovalProcessTable = DocBuilderApprovalProcess.buildIndexTable('../approvalProcesses/', this.approvalProcessesDescriptions, objectName);
       await replaceInFile(objectMdFile, '<!-- ApprovalProcess table -->', relatedApprovalProcessTable.join("\n"));
+      // Assignment Rules table
+      const relatedAssignmentRulesTable = DocBuilderAssignmentRules.buildIndexTable('../assignmentRules/', this.assignmentRulesDescriptions, objectName);
+      await replaceInFile(objectMdFile, '<!-- AssignmentRules table -->', relatedAssignmentRulesTable.join("\n"));
 
       this.objectDescriptions.push({
         name: objectName,
