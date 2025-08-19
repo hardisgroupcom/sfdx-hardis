bf665a294595f8622bbca98a9fb83494cbe684a2
Stepan Stepanov <stepan.stepanov@new.de>
2025-08-19 09:53:51 +0200

    Add todo

--- src/commands/hardis/project/deploy/smart.ts
+++ src/commands/hardis/project/deploy/smart.ts
@@ -483,6 +483,7 @@ If testlevel=RunRepositoryTests, can contain a regular expression to keep only c
       // Update package.xml
       const diffPackageXml = path.join(tmpDir, 'package', 'package.xml');
 
+      // TODO: enable by feature flag
       await extendPackageFileWithDependencies(diffPackageXml);
 
       await removePackageXmlContent(this.packageXmlFile, diffPackageXml, true, {
