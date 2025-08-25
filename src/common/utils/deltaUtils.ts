ade24fd1f2efd7a3d2547cbf834db88750c7d462
Stepan Stepanov <stepan.stepanov@new.de>
2025-08-25 18:57:15 +0200

    Refactor using parsePackageXmlFile and writePackageXmlFile

--- src/common/utils/deltaUtils.ts
+++ src/common/utils/deltaUtils.ts
@@ -1,59 +1,33 @@
 
-import { parseXmlFile, writeXmlFile } from './xmlUtils.js';
+import { parsePackageXmlFile, writePackageXmlFile } from './xmlUtils.js';
 
 type PackageType = {
   members: string[];
-  name: string[];
+  name: string;
 };
 
-type Cache = {
-  recordTypes: PackageType | null | undefined;
-  customFields: PackageType | null | undefined;
-  customMetadata: PackageType | null | undefined;
-  allLanguagesParsed: Array<string> | null;
-  fullPackageTypes: Array<PackageType> | null;
-};
+let fullPackageTypes = null;
 
-const cache: Cache = {
-  allLanguagesParsed: null,
-  fullPackageTypes: null,
-  recordTypes: null,
-  customFields: null,
-  customMetadata: null,
-};
-
-const allTypes = async (fullPackageFile: string): Promise<Array<PackageType>> => {
-  if (cache.fullPackageTypes) {
-    return cache.fullPackageTypes;
+const allTypes = async (fullPackageFile: string): Promise<Map<string, string[]>> => {
+  if (fullPackageTypes) {
+    return fullPackageTypes;
   }
 
-  cache.fullPackageTypes = (await parseXmlFile(fullPackageFile)).Package.types;
-  return cache.fullPackageTypes ?? [];
+  fullPackageTypes = await parsePackageXmlFile(fullPackageFile);
+  return fullPackageTypes ?? new Map();
 };
 
 const allLanguages = async (fullPackageFile: string): Promise<Array<string>> => {
-  if (cache.allLanguagesParsed) {
-    return cache.allLanguagesParsed;
-  }
-
-  cache.allLanguagesParsed = (await allTypes(fullPackageFile)).find(type => type.name[0] === "Translations")?.members ?? [];
-  return cache.allLanguagesParsed;
+ 
+  return (await allTypes(fullPackageFile))["Translations"] ?? [];
 };
 
-const addTypeIfMissing = (types: Array<PackageType>, typeToAdd: PackageType): boolean => {
+const addTypeIfMissing = (types: Map<string, string[]>, typeToAdd: PackageType) => {
   if (typeToAdd === null) {
-    return false;
+    return;
   }
 
-  const existingNodeByName = types.find(type => type.name[0] === typeToAdd.name[0]);
-  if (!existingNodeByName) {
-    types.push(typeToAdd);
-    return true;
-  } else {
-    const countBefore = existingNodeByName.members.length;
-    existingNodeByName.members = [...new Set([...existingNodeByName.members, ...typeToAdd.members])];
-    return countBefore !== existingNodeByName.members.length;
-  }
+  types[typeToAdd.name] = [...new Set([...(types[typeToAdd.name] ?? []), ...typeToAdd.members])];
 };
 
 export async function extendPackageFileWithDependencies(
@@ -71,7 +45,7 @@ export async function extendPackageFileWithDependencies(
       return null;
     }
     const languages = await allLanguages(fullPackageFile);
-    return { members: languages.map(languageSuffix => sobject + "-" + languageSuffix), name: ["CustomObjectTranslation"] };
+    return { members: languages.map(languageSuffix => sobject + "-" + languageSuffix), name: "CustomObjectTranslation" };
   };
 
   const dashSeparatedObjectToObjectTranslation = async (member: string): Promise<PackageType | null> => {
@@ -81,17 +55,17 @@ export async function extendPackageFileWithDependencies(
     }
     const sobject = parts[0];
     const languages = await allLanguages(fullPackageFile);
-    return { members: languages.map(languageSuffix => sobject + "-" + languageSuffix), name: ["CustomObjectTranslation"] };
+    return { members: languages.map(languageSuffix => sobject + "-" + languageSuffix), name: "CustomObjectTranslation" };
   };
 
   const objectTranslations = async (member: string): Promise<PackageType> => {
     const languages = await allLanguages(fullPackageFile);
-    return { members: languages.map(suffix => member + "-" + suffix), name: ["CustomObjectTranslation"] };
+    return { members: languages.map(suffix => member + "-" + suffix), name: "CustomObjectTranslation" };
   };
 
   const globalTranslations = async () => {
     const languages = await allLanguages(fullPackageFile);
-    return { members: languages, name: ["Translations"] };
+    return { members: languages, name: "Translations" };
   }
 
   const allCustomMetadataRecords = async (member: string): Promise<PackageType | null> => {
@@ -100,23 +74,17 @@ export async function extendPackageFileWithDependencies(
     }
     const baseName = member.split('__mdt')[0];
     const types = await allTypes(fullPackageFile);
-    const metadataRecords = 
-      (cache.customMetadata ?? (cache.customMetadata = types.find(type => type.name[0] === "CustomMetadata")))
-      ?.members
-      .filter(member => member.startsWith(baseName));
+    const metadataRecords = types["CustomMetadata"]?.filter(member => member.startsWith(baseName));
 
-    return metadataRecords?.length ? { members: metadataRecords, name: ["CustomMetadata"] } : null;
+    return metadataRecords?.length ? { members: metadataRecords, name: "CustomMetadata" } : null;
   }
 
   const allCustomFields = async (member: string): Promise<PackageType | null> => {
     const baseName = member.split('.')[0];
     const types = await allTypes(fullPackageFile);
-    const metadataFields = 
-      (cache.customFields ?? (cache.customFields = types.find(type => type.name[0] === "CustomField")))
-      ?.members
-      .filter(field => field.startsWith(baseName));
+    const metadataFields = types["CustomField"]?.filter(field => field.startsWith(baseName));
 
-    return metadataFields?.length ? { members: metadataFields, name: ["CustomField"] } : null;
+    return metadataFields?.length ? { members: metadataFields, name: "CustomField" } : null;
   }
 
   const allObjectRecordTypes = async (member: string): Promise<PackageType | null> => {
@@ -131,12 +99,9 @@ export async function extendPackageFileWithDependencies(
     const types = await allTypes(fullPackageFile);
     const sobject = parts[0];
 
-    const recordTypesMembers =
-      (cache.recordTypes ?? (cache.recordTypes = types.find(type => type.name[0] === "RecordType")))
-      ?.members
-      .filter(member => member.startsWith(sobject + '.'));
+    const recordTypesMembers = types["RecordType"]?.filter(member => member.startsWith(sobject + '.'));
 
-    return recordTypesMembers?.length ? { members: recordTypesMembers, name: ["RecordType"] } : null;
+    return recordTypesMembers?.length ? { members: recordTypesMembers, name: "RecordType" } : null;
   };
 
   const metadataProcessors = {
@@ -153,25 +118,20 @@ export async function extendPackageFileWithDependencies(
     "CustomField" : [allObjectRecordTypes, allCustomMetadataRecords, dotSeparatedObjectToObjectTranslation],
   };
   
-  const xml = await parseXmlFile(deltaXmlFile);
+  const parsedTypes = await parsePackageXmlFile(deltaXmlFile);
+  const clonedTypes = structuredClone(parsedTypes);
 
-  if (!xml?.Package?.types?.length) {
-    return;
-  }
-
-  const clonedTypes = [...xml.Package.types];
-
-  for (const typeNode of clonedTypes) {
-    const metadataType = typeNode.name[0];
+  for (const metadataType in clonedTypes) {
+    const members = clonedTypes[metadataType];
     if (Object.hasOwn(metadataProcessors, metadataType)) {
-      for (const member of typeNode.members) {
+      for (const member of members) {
         const processors = Array.isArray(metadataProcessors[metadataType]) ? metadataProcessors[metadataType] : [metadataProcessors[metadataType]];
         for (const processor of processors) {
-          addTypeIfMissing(xml.Package.types, await processor(member));
+          addTypeIfMissing(parsedTypes, await processor(member));
         }
       }
     }
   }
 
-  await writeXmlFile(deltaXmlFile, xml);
+  await writePackageXmlFile(deltaXmlFile, parsedTypes);
 }
