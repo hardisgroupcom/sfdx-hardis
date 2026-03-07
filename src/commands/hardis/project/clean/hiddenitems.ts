130aaa8295d0bfdf01cbaa5a858346a3a6d45d7f
Salik Lennert Pedersen <saliklp@gmail.com>
2026-03-07 14:52:34 +0100

    OS agnostic glob search (#1768)
    Co-authored-by: Salik Lennert Pedersen <slpe@nti-group.com>

--- src/commands/hardis/project/clean/hiddenitems.ts
+++ src/commands/hardis/project/clean/hiddenitems.ts
@@ -79,19 +79,20 @@ The command's technical implementation involves:
     uxLog("action", this, c.cyan(`Removing hidden dx managed source files`));
     /* jscpd:ignore-end */
     const rootFolder = path.resolve(this.folder);
-    const findManagedPattern = rootFolder + `/**/*.{app,cmp,evt,tokens,html,css,js,xml}`;
-    const matchingCustomFiles = await glob(findManagedPattern, { cwd: process.cwd(), ignore: GLOB_IGNORE_PATTERNS });
+    const findManagedPattern = '**/*.{app,cmp,evt,tokens,html,css,js,xml}';
+    const matchingCustomFiles = await glob(findManagedPattern, { cwd: rootFolder, ignore: GLOB_IGNORE_PATTERNS });
     let counter = 0;
     for (const matchingCustomFile of matchingCustomFiles) {
-      if (!fs.existsSync(matchingCustomFile)) {
+      const matchingCustomFilePath = path.join(rootFolder, matchingCustomFile);
+      if (!fs.existsSync(matchingCustomFilePath)) {
         continue;
       }
-      const fileContent = await fs.readFile(matchingCustomFile, 'utf8');
+      const fileContent = await fs.readFile(matchingCustomFilePath, 'utf8');
       if (fileContent.startsWith('(hidden)')) {
-        const componentFolder = path.dirname(matchingCustomFile);
+        const componentFolder = path.dirname(matchingCustomFilePath);
         const folderSplit = componentFolder.split(path.sep);
         const toRemove =
-          folderSplit.includes('lwc') || folderSplit.includes('aura') ? componentFolder : matchingCustomFile;
+          folderSplit.includes('lwc') || folderSplit.includes('aura') ? componentFolder : matchingCustomFilePath;
         await fs.remove(toRemove);
         uxLog("action", this, c.cyan(`Removed hidden item ${c.yellow(toRemove)}`));
         counter++;
